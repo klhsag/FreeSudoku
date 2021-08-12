7702bcc65ccc8448c5d1da5bd03cba3c20e16913
klhsag <klh@DESKTOP-8Q8CLK0.localdomain>
2021-08-12 16:34:00 +0800

    fix wrong ways

--- www/js/mydomgen.js
+++ www/js/mydomgen.js
@@ -1,10 +1,12 @@
 "use strict"
 
-function generateDomString(type, id, class_list, content, extra){
-    const str_id = (id) ? `id="${id}"` : ""
+function generateDomString(type, id, class_list, content){
+    const block = document.createElement(type)
+    block.id = id
     const str_class_inner = class_list.join(' ')
-    const str_class = (str_class_inner) ? `class="${str_class_inner}"` : ""
-    return `<${type} ${str_id} ${str_class} ${extra}>${content}</${type}>`
+    block.className = str_class_inner
+    block.innerHTML = content
+    return block
 }
 
 function getDivHtml(content="", class_list=[], id=""){
