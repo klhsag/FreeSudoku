7a7bc40af0afd5b49f5970990cbf882e8f3a3b18
klhsag <klh@DESKTOP-8Q8CLK0.localdomain>
2021-08-16 14:37:49 +0800

    add toolbar...

--- www/js/sudoku-toolbar.js
+++ www/js/sudoku-toolbar.js
@@ -2,9 +2,13 @@
 
 class SudokuToolbar{
     constructor(){
-        this._btnsrc = Set()
+        this._btnsrc = new Set()
         for (let i=1; i<=9; ++i){
             this._btnsrc.add(new SudokuNum(i))
+        }
+        this.dom = createDivBlock("", ["sudoku-toolbar"])
+        for (const btnsrc of this._btnsrc){
+            this.dom = createDivBlock(String(sym2num(btnsrc.val)), ["sudoku-toolbar-grid"])
         }
     }
 }
